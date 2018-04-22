25e81575afff5a32fbbc9bb5bcbc4cb991f9a61e
Ankit Pant <ankit12.pant@gmail.com>
2018-04-23 00:27:16 +0530

    section2 - lesson 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { User } from './model/user';
+import { Observable } from 'rxjs/observable';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
+  constructor(private authService: AuthService) {
 
+  }
 
-
-
-
-
-
-
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+    this.isLoggedOut$ = this.authService.isLoggedOut$;
+  }
 }
